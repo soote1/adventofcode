42982be9a14f49393b34b706fb134ea52881bda0
Isaias Ramirez <soote1.ir@gmail.com>
2023-01-24 00:36:44 -0600

    Solve day 15 part 1
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- 2022/beacon-exclusion-zone/main.go
+++ 2022/beacon-exclusion-zone/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -12,27 +14,91 @@ type Point struct {
 	y int
 }
 
-type ExclusionZone struct {
-	rows map[int][]Point
+type Sensor struct {
+	location      Point
+	closestBeacon Beacon
 }
 
-func calculateExclusionZone(sensors []Point, beacons []Point) *ExclusionZone {
-	exclusionZone := ExclusionZone{}
+type Beacon struct {
+	location Point
+}
+
+type Segment struct {
+	start Point
+	end   Point
+}
+
+type ExclusionZone struct {
+	rows map[int][]Segment
+}
+
+func calculateExclusionZone(sensors []Sensor) *ExclusionZone {
+	var d int
+	exclusionZone := ExclusionZone{rows: make(map[int][]Segment)}
 	for i := 0; i < len(sensors); i++ {
-		// TODO: calculate distance to closest beacon
-		// TODO: Save corresponding range for upper rows (from sensor coordinates)
-		// TODO: Save corresponding range for lower rows (from sensor coordinates)
-		// TODO: Save corresponding range for current row (from sensor coordinates)
+		s := sensors[i]
+		xdiff := int(math.Abs(float64(s.location.x - s.closestBeacon.location.x)))
+		ydiff := int(math.Abs(float64(s.location.y - s.closestBeacon.location.y)))
+		d = xdiff + ydiff
+		for x, y := d-1, s.location.y-1; x > 0; y, x = y-1, x-1 {
+			segment := Segment{
+				start: Point{x: s.location.x - x},
+				end:   Point{x: s.location.x + x},
+			}
+			exclusionZone.rows[y] = append(exclusionZone.rows[y], segment)
+		}
+		for x, y := d-1, s.location.y+1; x > 0; x, y = x-1, y+1 {
+			segment := Segment{
+				start: Point{x: s.location.x - x},
+				end:   Point{x: s.location.x + x},
+			}
+			exclusionZone.rows[y] = append(exclusionZone.rows[y], segment)
+		}
+		segment := Segment{
+			start: Point{x: s.location.x - d},
+			end:   Point{x: s.location.x + d},
+		}
+		exclusionZone.rows[s.location.y] = append(
+			exclusionZone.rows[s.location.y],
+			segment,
+		)
+
 	}
 	return &exclusionZone
 }
 
-func countExcludedCells(ez *ExclusionZone, beacons []Point, row int) int {
+func countExcludedCells(ez *ExclusionZone, s []Sensor, row int) int {
+	beacons := make(map[Beacon]bool)
 	count := 0
-	// TODO: calculate min Point
-	// TODO: calculate max Point
-	// TODO: count = max-min
-	// TODO: remove beacons in row from count
+	segments := ez.rows[row]
+	sort.Slice(segments, func(i, j int) bool {
+		return segments[i].start.x < segments[j].start.x
+	})
+	nonOverlappingSegments := []Segment{}
+	currentSegment := segments[0]
+	for i := 1; i < len(segments); i++ {
+		if currentSegment.end.x < segments[i].start.x {
+			nonOverlappingSegments = append(nonOverlappingSegments, currentSegment)
+			currentSegment = segments[i]
+		} else if currentSegment.end.x >= segments[i].start.x && currentSegment.end.x <= segments[i].end.x {
+			currentSegment.end = segments[i].end
+		}
+		if i == len(segments)-1 {
+			nonOverlappingSegments = append(nonOverlappingSegments, currentSegment)
+		}
+	}
+	for _, segment := range nonOverlappingSegments {
+		count += (segment.end.x - segment.start.x) + 1
+	}
+	for _, sensor := range s {
+		if _, ok := beacons[sensor.closestBeacon]; !ok {
+			if sensor.closestBeacon.location.y == row {
+				count--
+			}
+			beacons[sensor.closestBeacon] = true
+		}
+	}
+
 	return count
 }
 
@@ -49,26 +115,25 @@ func collectNumber(offset *int, data string) int {
 	return number
 }
 
-func parseInput(input []string) ([]Point, []Point) {
+func parseInput(input []string) []Sensor {
 	var offset int
-	sensors := []Point{}
-	beacons := []Point{}
+	sensors := []Sensor{}
 	for _, line := range input {
-		sensor := Point{}
-		beacon := Point{}
+		sensor := Sensor{}
+		beacon := Beacon{}
 		offset = 12
-		sensor.x = collectNumber(&offset, line)
+		sensor.location.x = collectNumber(&offset, line)
 		offset += 4
-		sensor.y = collectNumber(&offset, line)
+		sensor.location.y = collectNumber(&offset, line)
 		offset += 25
-		beacon.x = collectNumber(&offset, line)
+		beacon.location.x = collectNumber(&offset, line)
 		offset += 4
-		beacon.y = collectNumber(&offset, line)
+		beacon.location.y = collectNumber(&offset, line)
+		sensor.closestBeacon = beacon
 		sensors = append(sensors, sensor)
-		beacons = append(beacons, beacon)
 
 	}
-	return sensors, beacons
+	return sensors
 }
 
 func loadInput(fileName string) []string {
@@ -88,8 +153,14 @@ func loadInput(fileName string) []string {
 }
 
 func main() {
-	input := loadInput(os.Args[1])
-	sensors, beacons := parseInput(input)
-	fmt.Println(sensors)
-	fmt.Println(beacons)
+	inputFile := os.Args[1]
+	row, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		panic("need to provide row number")
+	}
+	input := loadInput(inputFile)
+	sensors := parseInput(input)
+	exclusionZone := calculateExclusionZone(sensors)
+	excludedCells := countExcludedCells(exclusionZone, sensors, row)
+	fmt.Println(excludedCells)
 }
